73be703b7044f75a79f8aa2b4b808e002e555a35
Andrey Voronin <AVor0n@yandex.ru>
2021-12-08 11:31:06 +0500

    feat: replace 'eval' with 'pipeline'

--- my_ciphering_cli.js
+++ my_ciphering_cli.js
@@ -1,52 +1,63 @@
 'use strict';
-const fs = require('fs');
-const process = require('process');
-const caesarCipherCodeStream = require('./caesarCipherCodeStream');
-const caesarCipherDecodeStream = require('./caesarCipherDecodeStream');
-const rot8CodeStream = require('./rot8CodeStream');
-const rot8DecodeStream = require('./rot8DecodeStream');
-const atbashCodeStream = require('./atbashCodeStream');
+const fs = require("fs");
+const { pipeline } = require("stream");
+const { promisify } = require("util");
+const process = require("process");
+const caesarCipherCodeStream = require("./caesarCipherCodeStream");
+const caesarCipherDecodeStream = require("./caesarCipherDecodeStream");
+const rot8CodeStream = require("./rot8CodeStream");
+const rot8DecodeStream = require("./rot8DecodeStream");
+const atbashCodeStream = require("./atbashCodeStream");
+
+const pipelineAsync = promisify(pipeline);
 
 function getValue(flag) {
   const flagIndex = process.argv.indexOf(flag);
   return flagIndex !== -1 ? process.argv[flagIndex + 1] : null;
 }
 
-const config = getValue('-c') || getValue('--config');
-const input = getValue('-i') || getValue('--input');
-const output = getValue('-o') || getValue('--output');
+const config = getValue("-c") || getValue("--config");
+const input = getValue("-i") || getValue("--input");
+const output = getValue("-o") || getValue("--output");
 
 function parseConfig(config) {
   const streams = [];
-  const actions = config.split('-');
+  const actions = config.split("-");
   for (let action of actions) {
     switch (action) {
-      case 'C0':
-        streams.push('(new caesarCipherDecodeStream())');
+      case "C0":
+        streams.push(new caesarCipherDecodeStream());
         break;
-      case 'C1':
-        streams.push('(new caesarCipherCodeStream())');
+      case "C1":
+        streams.push(new caesarCipherCodeStream());
         break;
-      case 'A':
-      case 'A0':
-      case 'A1':
-        streams.push('(new atbashCodeStream())');
+      case "A":
+      case "A0":
+      case "A1":
+        streams.push(new atbashCodeStream());
         break;
-      case 'R0':
-        streams.push('(new rot8DecodeStream())');
+      case "R0":
+        streams.push(new rot8DecodeStream());
         break;
-      case 'R1':
-        streams.push('(new rot8CodeStream())');
+      case "R1":
+        streams.push(new rot8CodeStream());
         break;
       default:
-        throw new Error('Неверный конфиг. Получено: ' + action);
+        throw new Error("Неверный конфиг. Получено: " + action);
     }
   }
-  return streams.join('.pipe');
+  return streams;
 }
 
-let task = input ? 'fs.createReadStream(input, "utf8")' : 'process.stdin';
-task += '.pipe' + parseConfig(config) + '.pipe';
-task += output ? '(fs.createWriteStream(output))' : '(process.stdout)';
+const inStream = input ? fs.createReadStream(input, "utf8") : process.stdin;
+const transStream = parseConfig(config);
+const outStream = output ? fs.createWriteStream(output) : process.stdout;
 
-eval(task);
+
+(async function run() {
+  try {
+    await pipelineAsync(inStream, ...transStream, outStream);
+  } catch (err) {
+    console.error("pipeline failed with error:", err);
+  }
+})();
